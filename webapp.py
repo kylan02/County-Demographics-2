3905158bd8e3cedea61597d67e1190080f23d054
kylan02 <31520026+kylan02@users.noreply.github.com>
2019-11-01 09:45:52 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,3 +1,10 @@
 from flask import Flask, request, Markup, render_template, flash, Markup
 import os
 import json
+
+app = Flask(__name__)
+
+#body
+
+if __name__=="__main__":
+    app.run(debug=False)
