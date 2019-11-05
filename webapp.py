567bfdad34b63ab7112793a54d9605de5d99cae5
Kylan O'Connor <kylan02@gmail.com>
2019-11-05 10:00:25 -0800

    commit

--- webapp.py
+++ webapp.py
@@ -4,7 +4,21 @@ import json
 
 app = Flask(__name__)
 
-#body
+@app.route("/")
+def render_main():
+	with open('county_demographics.json') as demographics_data:
+		counties = json.load(demographics_data)
+	return render_template('home.html', states = get_state_options(counties))
+
+def get_state_options(counties):
+	listOfStates = []
+	for data in counties:
+		if data['State'] not in listOfStates:
+			listOfStates.append(data['State'])
+	options = ""
+	for data in listOfStates:
+		options = options + Markup("<option value=\""+data+"\">"+data+"</option>")
+	return options
 
 if __name__=="__main__":
     app.run(debug=False)
