c99a63c05e4eda773831ceb403e9e1d73ea41a5c
Kylan O'Connor <kylan02@gmail.com>
2019-11-12 10:01:03 -0800

    commit

--- webapp.py
+++ webapp.py
@@ -9,7 +9,7 @@ def render_main():
 	with open('county_demographics.json') as demographics_data:
 		counties = json.load(demographics_data)
 	if 'states' in request.args:
-		return render_template('home.html', states = get_state_options(counties), highestTravelTime = fun_fact(request.args['states']))
+		return render_template('home.html', states = get_state_options(counties), highestTravelTime = fun_fact(request.args['states'], counties))
 	else:
 		return render_template('home.html', states = get_state_options(counties))
 
@@ -23,13 +23,14 @@ def get_state_options(counties):
 		options = options + Markup("<option value=\""+data+"\">"+data+"</option>")
 	return options
 
-def fun_fact(counties):
+def fun_fact(state, counties):
 		#county with the longest average travel time
-	highestTravelTime = ["county", 0]
+	highestTravelTime = ["county", 0.0]
 	for data in counties:
-		if highestTravelTime[1] < data['Miscellaneous']['Mean Travel Time to Work']:
-			highestTravelTime[1] = data['Miscellaneous']['Mean Travel Time to Work']
-			highestTravelTime[0] = data['County']
+		if data['State'] == state:
+			if highestTravelTime[1] < data['Miscellaneous']['Mean Travel Time to Work']:
+				highestTravelTime[1] = data['Miscellaneous']['Mean Travel Time to Work']
+				highestTravelTime[0] = data['County']
 	return highestTravelTime
 if __name__=="__main__":
     app.run(debug=False)
